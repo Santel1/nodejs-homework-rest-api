d3b7a731e7f62b6b0993a92827d7fcc004d1dcdc
Santel1 <126976920+Santel1@users.noreply.github.com>
2023-12-13 17:54:01 +0100

    hw-03

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 
+const mongoose = require("mongoose");
+
 dotenv.config({
   path:
     process.env.NODE_ENV === "development"
@@ -8,6 +10,16 @@ dotenv.config({
       : "./envs/prod.env",
 });
 
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then((con) => {
+    console.log(con);
+    console.log("MongoDB connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
 console.log(process.env.NODE_ENV);
 
 const port = process.env.PORT ?? 4000;
